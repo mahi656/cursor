6eaae1d90f042a9088ff5ee859e80060d1990e1e
mahi656 <mahisawner1234@gmail.com>
2025-04-19 13:12:21 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import TrailingCursor from "./components/TrailingCursor";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="app">
+      <TrailingCursor />
+    </div>
+  );
+}
+
+export default App;
